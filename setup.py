4104228f331eeb2219ac00b027718e5b54b3ea28
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-10-10 16:22:45 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,43 +1,22 @@
 import os
 import sys
-import json
 
-REQUIRED_KEYS = set(['bot_token', 'app_id'])
+def kill():
+	os.system("ps aux | grep ython | grep fb_2_telegram | awk '{print $2}' | xargs kill -9")
 
-def setup(arg = ''):
-	RUN_COMMAND = 'nohup python3 -u post_fb.py &'
+def setup():
+	kill()
+	if 'kill' in str(sys.argv):
+		return 
 
-	CREDENTIALS = {}
-	try:
-		with open('CREDENTIALS') as f:
-			CREDENTIALS = json.load(f)
-	except:
-		pass
-
-	for key in REQUIRED_KEYS:
-		if key not in CREDENTIALS:
-			print('ERROR: please fill the CREDENTIALS file in json format, required keys : ' + ', '.join(sorted(REQUIRED_KEYS)))
-			return
-
-	if arg != 'reload' and arg != 'debug':
-		os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-		os.system('sudo python3 get-pip.py')
-		os.system('rm get-pip.py')
-
-		os.system('sudo pip3 install -r requirements.txt')
-		os.system('sudo pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
-			
-	# kill the old running bot if any. If you need two same bot running in one machine, use mannual command instead
-	os.system("ps aux | grep ython | grep 'post_fb.py' | awk '{print $2}' | xargs kill -9")
-
-	if arg == 'debug':
-		os.system(RUN_COMMAND[6:-2])
+	if 'debug' in str(sys.argv):
+		os.system('python3 -u fb_2_telegram.py')
 	else:
-		os.system(RUN_COMMAND)
+		os.system('nohup python3 -u fb_2_telegram.py %s &' % ' '.join(sys.argv[1:])) 
+		if 'notail' not in sys.argv:
+			os.system('touch nohup.out')
+			os.system('tail -F nohup.out')
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		setup(sys.argv[1])
-	else:
-		setup('')
+	setup()
