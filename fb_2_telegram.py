5d22623b7cc251154bb3f2e93677e7f5832fb094
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-01-10 14:44:47 -0800

    commit

--- fb_2_telegram.py
+++ fb_2_telegram.py
@@ -5,4 +5,7 @@ import facebook
 with open('credential') as f:
 	credential = yaml.load(f, Loader=yaml.FullLoader)
 fb_graph = facebook.GraphAPI(credential['access_token'])
-fb_graph.get_object('transarmy')
+fb_graph.put_object(
+	parent_object="439266300053488",
+	connection_name="feed",
+	message="test")
